74d104a3f62555bc94d2e797bd8f51063cd46464
Sachin__Mani <157294174+Sachin9600466711@users.noreply.github.com>
2026-02-15 15:34:37 +0530

    Removed Unnecessary Imports

--- src/main/java/org/example/asyncthreestep/executor/ExecutorImplementation.java
+++ src/main/java/org/example/asyncthreestep/executor/ExecutorImplementation.java
@@ -13,8 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronization;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springframework.web.client.RestTemplate;
 import tools.jackson.databind.ObjectMapper;
 
